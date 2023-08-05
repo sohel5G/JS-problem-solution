3af3cceb6b62b2f6fc3f8a2ed77d61ac9947d1d3
sohel5G <contactsohelrana@gmail.com>
2023-08-05 17:22:50 +0600

    before creating temp file

---
+++ math.js
@@ -0,0 +1,11 @@
+// console.log( Math.pow(2, 5) );
+// console.log( Math.abs(-2) );
+// console.log( Math.round(23.3456) );
+// console.log( Math.ceil(2.00001) );
+// console.log( Math.floor(2.99999) );
+console.log( Math.random() );
+
+
+
+
+
---
+++ string.js
@@ -0,0 +1,23 @@
+let str = 'Hello World one two';
+let str2 = 'For developer';
+
+// console.log( str.toLocaleLowerCase() );
+// console.log( str2.toLowerCase() );
+// console.log(str.search('llo'));
+// console.log( str.includes('wor') );
+// console.log( str.indexOf('Wo') );
+// console.log( str.startsWith('Hello') );
+// console.log( str.endsWith('ld') );
+// console.log( str.split('') );
+// console.log( str.slice( 2, 5 ) );
+// console.log( str2.trim() );
+// console.log( str2.trimEnd() );
+// console.log(str.concat( ' ' , str2 ));
+
+let exArray = ['One', 'Two', 'Three', 'Four'];
+
+console.log( exArray.join(' | ') );
+
+
+
+
